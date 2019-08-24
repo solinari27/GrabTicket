c03ef44e7fad65b5039d80eb6ea1af6f34991119
solinari <solinari27@gmail.com>
2019-08-24 21:16:34 +0800

    login html parse

--- src/robot/login.py
+++ src/robot/login.py
@@ -21,37 +21,26 @@ class Login():
         pass
 
 
-class NewParser(HTMLParser):  # 继承基类HTMLParser，同时改写基类几个函数，主要还是为了打印的目的。Sax模式
-    def handle_decl(self, decl):
-        HTMLParser.handle_decl(self, decl)
-        print('decl %s' % decl)
+class LoginParser(HTMLParser):  # 继承基类HTMLParser，同时改写基类几个函数，主要还是为了打印的目的。Sax模式
+    def __init__(self):
+        HTMLParser.__init__(self)
+        # self.links = []
 
     def handle_starttag(self, tag, attrs):
-        HTMLParser.handle_starttag(self, tag, attrs)
-        print('<' + tag + '>')
-
-    def handle_endtag(self, tag):
-        HTMLParser.handle_endtag(self, tag)
-        print('</' + tag + '>')
+        if tag == 'div':
+            print attrs
 
     def handle_data(self, data):
-        HTMLParser.handle_data(self, data)
-        print('data %s' % data)
-
-    # 下面的代码是处理类似于<br/>这样的没有闭合的标签
-    def handle_startendtag(self, tag, attrs):
-        HTMLParser.handle_startendtag(self, tag, attrs)
-
-    def handle_comment(self, data):
-        HTMLParser.handle_comment(self, data)
-        print('data %s' % data)
+        print (data)
 
     def close(self):
         HTMLParser.close(self)
-        print('Close')
 
 
 url = 'https://guahao.zjol.com.cn/login'
 resp = urllib2.urlopen(url)
 page = resp.read()
-print (page)
+# print (page)
+parser = LoginParser()
+parser.feed(page)
+parser.close()
